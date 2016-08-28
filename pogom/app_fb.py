ddf9ab3fb3d920f6338247a26a1ce6baebb64f6f
ChengYang_Wu <chengyang_wu@trend.com.tw>
2016-08-28 10:01:32 +0800

    message for basic information

--- pogom/app_fb.py
+++ pogom/app_fb.py
@@ -2,6 +2,8 @@ from . import config
 from .app import Pogom
 from .utils import get_pokemon_id
 from flask import request
+from datetime import datetime
+from time import time
 import logging
 import requests
 import json
@@ -17,7 +19,9 @@ class PogomFb(Pogom):
         super(PogomFb, self).__init__(*args, **kwargs)
         self.route('/fb', methods=['POST'])(self.message_handler)
         self.route('/fb', methods=['GET'])(self.verify)
+        # move to model or somewhere
         self._fb_subscribers = config['FB_SUBSCRIBERS'] or {}
+        self._fb_noti_history = {}
 
     def verify(self):
         log.info('')
@@ -43,11 +47,50 @@ class PogomFb(Pogom):
         return "ok", 200
 
     def notify(self, pokemon_list):
-        log.debug(pokemon_list)
         for recipient, notify_when_found in self._fb_subscribers.iteritems():
-            for m in pokemon_list:
-                if m in notify_when_found:
-                    fb_send_message(recipient, "{0} is shown and he's going to fuck you".format(m))
+            for msg in self._generate_notify_msg(recipient, notify_when_found, pokemon_list):
+                fb_send_message(recipient, msg)
+
+    def _generate_notify_msg(self, recipient, notify_list, pokemon_list):
+        for m in pokemon_list:
+            if m["pokemon_id"] not in notify_list:
+                continue
+            if m["encounter_id"] not in self._fb_noti_history[recipient]:
+                self._fb_noti_history[recipient][m["encounter_id"]] = m['disappear_time']
+                exp_ctime = "{h}:{m}:{s}".format(
+                    m['disappear_time'].hour, m['disappear_time'].minute,
+                    m['disappear_time'].second)
+                msg = (
+                    "A wild {pokemon_name} appeared!",
+                    "Disappeared at ({ctime})",
+                    "longitude:{longitude}, latitude:{latitude}"
+                )
+                msg = "\n".join(msg)
+                msg = msg.format(
+                    pokemon_name=m['pokemon_name'],
+                    longitude=m['longitude'],
+                    latitude=m['latitude'],
+                    ctime=exp_ctime
+                )
+                yield msg
+
+    def _clear_expired_entries_from_history(self):
+        def _get_timestamp(dt):
+            return (dt - datetime(1970, 1, 1)).total_seconds()
+        pass
+
+    def _init_subscriber(self, s_id):
+        self._fb_subscribers[s_id] = []
+        self._fb_noti_history[s_id] = {}
+
+    def _subscribe_pokemon(self, s_id, pokemon_id):
+        self._fb_subscribers[s_id].append(pokemon_id)
+        self._save_subscriber()
+
+    def _unsubscribe(self, s_id):
+        del self._fb_subscribers[s_id]
+        del self._fb_noti_history[s_id]
+        self._save_subscriber()
 
     def _save_subscriber(self):
         if (config['CONFIG_PATH'] is not None and os.path.isfile(config['CONFIG_PATH'])):
@@ -63,18 +106,16 @@ class PogomFb(Pogom):
     def _message_processor(self, sender_id, msg):
         response_msg = "QQ more"
         if 'forget me' in msg and sender_id in self._fb_subscribers:
-            del self._fb_subscribers[sender_id]
-            self._save_subscriber()
+            self._unsubscribe(sender_id)
             response_msg = "how sad but I will..."
         elif 'tell me about' in msg:
             if sender_id not in self._fb_subscribers:
-                self._fb_subscribers[sender_id] = []
+                self._init_subscriber(sender_id)
             pokemon_name = msg.split('tell me about')[1].strip()
             pokemon_id = get_pokemon_id(pokemon_name)
             if pokemon_id:
                 if pokemon_id not in self._fb_subscribers[sender_id]:
-                    self._fb_subscribers[sender_id].append(pokemon_id)
-                    self._save_subscriber()
+                    self._subscribe_pokemon(sender_id, pokemon_id)
                 response_msg = "sure bro"
             else:
                 response_msg = u"wat's {0}".format(pokemon_name)
